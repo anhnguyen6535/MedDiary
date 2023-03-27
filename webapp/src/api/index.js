9fc33071ab15134e2a8dfa93b2e460046e58a403
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-03-27 03:10:56 -0600

    Restrict user from unauthorized login

--- webapp/src/api/index.js
+++ webapp/src/api/index.js
@@ -4,8 +4,7 @@ export const BASE_URL = 'https://localhost:7113/';
 
 export const ENDPOINTS = {
     participant: 'participant',
-    question:'question',
-    getAnswers : 'question/getanswers'
+    user: 'Users',
 }
 
 export const createAPIEndpoint = endpoint => {
@@ -15,6 +14,7 @@ export const createAPIEndpoint = endpoint => {
         fetch: () => axios.get(url),
         fetchById: id => axios.get(url + id),
         post: newRecord => axios.post(url, newRecord),
+        log: info => axios.post(url, info),
         put: (id, updatedRecord) => axios.put(url + id, updatedRecord),
         delete: id => axios.delete(url + id),
     }
