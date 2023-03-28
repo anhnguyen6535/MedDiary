e6568c18fb15c20cb4d4e30a58951584480b42d4
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-03-27 22:44:34 -0600

    Modified Center Component

--- webapp/src/components/Center.js
+++ webapp/src/components/Center.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useWindowDimensions from '../hooks/useWindowDimensions';
 
 export default function Center(prop) {
   const {width, height} = useWindowDimensions()
 
+  // const color = prop.color? prop.color :'white'
+  
   const divStyle = {
     maxWidth: width,
     minHeight: height,
-    // backgroundColor: "red"
+    // border: '10px solid red',
+    // backgroundColor: color
   }
 
   return (
+
     <div className='container d-flex align-items-center justify-content-center' style={divStyle}>
       {prop.children}
     </div>
