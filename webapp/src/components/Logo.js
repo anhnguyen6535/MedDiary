0ac29862e61f40c6ef26762301fadd02318b3b65
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-03-29 14:43:34 -0600

    Added redirect home when clicked logo

--- webapp/src/components/Logo.js
+++ webapp/src/components/Logo.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Logo(prop) {
+  const navigate = useNavigate()
   const size = {
     fontSize: prop.size
   }
 
   return (
-    <div className='logo' style={size}>MedDiary</div>
+    <div className='logo' style={size} onClick={() => navigate('/')}>MedDiary</div>
   )
 }
