09f61e5893bd8eeae62ba94ffea207f3fca2d8d5
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-03-29 22:56:38 -0600

    Api called for login

--- webapp/src/api/index.js
+++ webapp/src/api/index.js
@@ -7,14 +7,14 @@ export const ENDPOINTS = {
     user: 'Users',
 }
 
-export const createAPIEndpoint = endpoint => {
+export const createAPIEndpoint = (endpoint) => {
 
     let url = BASE_URL + 'api/' + endpoint + '/';
     return {
         fetch: () => axios.get(url),
         fetchById: id => axios.get(url + id),
         post: newRecord => axios.post(url, newRecord),
-        log: info => axios.post(url, info),
+        log: info => axios.post(url + 'Login', info),
         put: (id, updatedRecord) => axios.put(url + id, updatedRecord),
         delete: id => axios.delete(url + id),
     }
