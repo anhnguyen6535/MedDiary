a8e998aba6f9730cc9e1472a47037b5cdd69bcf0
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-04-04 01:14:19 -0600

    Connect API LabResult

--- webapp/src/components/LabResults.js
+++ webapp/src/components/LabResults.js
@@ -1,37 +1,11 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
 import { ENDPOINTS } from '../api'
 import ArrayObjAPIProcessor from './ArrayObjAPIProcessor'
-import { filterObjList } from './data/helper'
-import { medicaionDTO } from './data/userDTO'
-import Medication from './Medication'
-import { VerticalTable } from './TableComponent'
+import { labDTO } from './patients/PatientDTO'
 
 export default function LabResults() {
-   // FIXME FAKE DATA
-  const labList = [
-    {
-        date: "28/03/2002",
-        lab: "ABC",
-        service: "Blood Type Test",
-        result: "A+",
-        notes: ""
-    },
-    {
-        date: "28/03/2002",
-        lab: "EFG",
-        service: "XRay",
-        result: "Clear",
-        notes: ""
-    },
-  ] 
-
-  const {headers, values} = filterObjList(labList)
 
   return (
-    <ArrayObjAPIProcessor apiPath={ENDPOINTS.medicaion} dtoFilter={(e) => medicaionDTO(e)}/>
-    // <Container>
-    //     <VerticalTable header={headers} val={values} />
-    // </Container>
+    <ArrayObjAPIProcessor apiPath={ENDPOINTS.lab} dtoFilter={(e) => labDTO(e)}/>
   )
 }
