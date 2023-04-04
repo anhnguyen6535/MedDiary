1806ddae1f0b96203619afd4759af286aadfa767
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-04-04 01:14:31 -0600

    Add LabDTO

--- webapp/src/components/patients/PatientDTO.js
+++ webapp/src/components/patients/PatientDTO.js
@@ -1,1 +1,13 @@
+import { filterObjList } from "../data/helper"
+
 export const profile = ['First name', 'Last name', 'Email', 'AHS Number']
+
+export const labDTO = (labLists) =>{
+    for(let i = 0; i < labLists.length; i++){
+        delete labLists[i].patientSin
+        delete labLists[i].testId
+    }
+
+    const {headers, values} = filterObjList(labLists)
+    return {headers, values}
+}
