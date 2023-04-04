8361302009f65f4cc54c79a12c340c1c129a3464
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-04-04 01:14:01 -0600

    Dynamic Medication

--- webapp/src/components/Medication.js
+++ webapp/src/components/Medication.js
@@ -1,34 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap';
-import { createAPIEndpoint, ENDPOINTS } from '../api';
-import useStateContext from '../hooks/useStateContext';
+import React from 'react'
+import { ENDPOINTS } from '../api';
+import ArrayObjAPIProcessor from './ArrayObjAPIProcessor';
 import { medicaionDTO } from './data/userDTO';
-import { VerticalTable } from './TableComponent';
 
 export default function Medication() {
-  const { context } = useStateContext();
-  const [headers, setHeaders] = useState([]);
-  const [values, setValues] = useState([]);
-
-  // load data from API
-  useEffect(() => {
-    createAPIEndpoint(ENDPOINTS.medicaion)
-      .fetchById(context.sin)
-      .then(res => {
-        const {headers, values} = medicaionDTO(res.data);
-        setHeaders(headers);
-        setValues(values);
-      })  
-      .catch(err => {
-        console.log(err);
-        // setSub(true)
-        // setValues({email: '', password: '', isDoctor: context.isDoctor})
-      })
-  }, [])
 
   return (
-    <Container>
-      <VerticalTable header={headers} val={values} />
-    </Container>
+    <ArrayObjAPIProcessor apiPath={ENDPOINTS.medicaion} dtoFilter={(e) => medicaionDTO(e)}/>
   )
 }
