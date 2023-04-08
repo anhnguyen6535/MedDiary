e305a09111751578da21434b842d2c0aabd682e8
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-04-08 17:08:48 -0600

    Deleted WorkFor from db

---
+++ Backend/Migrations/20230408230341_DeleteWorkFor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteWorkFor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Work_Fors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClinicPhone",
+                table: "Doctors",
+                type: "varchar(15)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClinicPhone",
+                table: "Doctors");
+
+            migrationBuilder.CreateTable(
+                name: "Work_Fors",
+                columns: table => new
+                {
+                    DoctorId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Phone = table.Column<string>(type: "varchar(15)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Work_Fors", x => x.DoctorId);
+                });
+        }
+    }
+}
