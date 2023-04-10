32f70b28f328dec6508b2df560863267f4ec32a9
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-04-10 02:53:14 -0600

    Cleaned General Context

--- Backend/Models/GeneralContext.cs
+++ Backend/Models/GeneralContext.cs
@@ -22,14 +22,7 @@ namespace Learning.Models
         public DbSet<EmergencyContact> EmergencyContacts { get; set; } = null!;
         public DbSet<Insurance> Insurances { get; set; } = null!;
         public DbSet<TodoList> TodoLists { get; set; } = null!;
-        public DbSet<Backend.Models.Labtest> Labtest { get; set; }
-        public DbSet<Backend.Models.ClinicVisit> ClinicVisits { get; set; }
-        
-        // // THESE MODELS FOR TESTING WILL BE REMOVED
-
-        // public DbSet<Person> Persons { get; set; } = null!;
-        // public DbSet<Question> Questions { get; set; } = null!;
-        // public DbSet<Participant> Participants { get; set; } = null!;
-
+        public DbSet<Labtest> Labtest { get; set; }
+        public DbSet<ClinicVisit> ClinicVisits { get; set; }        
     }
 }
